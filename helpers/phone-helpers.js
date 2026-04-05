91797054c5ab2165eb0a9291c268e637ff59f5e5
heykau57-lgtm <heykau57@gmail.com>
2026-04-05 15:53:31 +0800

    Add phone number validation and country code lookup

--- helpers/phone-helpers.js
+++ helpers/phone-helpers.js
@@ -1,1 +1,43 @@
+const countryCodes = {
+    '60': 'Malaysia',
+    '62': 'Indonesia',
+    '65': 'Singapore',
+    '66': 'Thailand',
+    '63': 'Philippines',
+    '84': 'Vietnam',
+    '95': 'Myanmar',
+    '855': 'Cambodia',
+    '856': 'Laos',
+    '673': 'Brunei',
+    '1': 'United States',
+    '44': 'United Kingdom',
+    '61': 'Australia',
+    '91': 'India'
+};
 
+function getCountryCode(phoneNumber) {
+    const clean = phoneNumber.replace(/\D/g, '');
+    for (const [code, country] of Object.entries(countryCodes)) {
+        if (clean.startsWith(code)) {
+            return country;
+        }
+    }
+    return 'Unknown';
+}
+
+function validatePhoneNumber(phoneNumber) {
+    const clean = phoneNumber.replace(/\D/g, '');
+    if (clean.length < 7) {
+        return { isValid: false, message: 'Nomor terlalu pendek' };
+    }
+    if (clean.length > 17) {
+        return { isValid: false, message: 'Nomor terlalu panjang' };
+    }
+    return { 
+        isValid: true, 
+        cleanNumber: clean,
+        country: getCountryCode(clean)
+    };
+}
+
+module.exports = { getCountryCode, validatePhoneNumber }; 
